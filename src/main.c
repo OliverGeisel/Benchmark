262628677551529d07ca4f969b9c9844ae80d2d7
Oliver <geisel.oliver@googlemail.com>
2018-05-04 15:28:44 +0200

    minor fixes and optimizations

--- src/main.c
+++ src/main.c
@@ -10,23 +10,31 @@
 
 int secondmain();
 
+uint32_t dim = 0;
+const uint32_t step = 32;
+const uint32_t max = 2048;
+
+
+void set_dim() {
+    if (dim < 256) { dim += step; }
+    else if (dim < 512) { dim += step * 2; }
+    else if (dim < 1024) { dim += step * 4; }
+    else if (dim < 2048) { dim += step * 8; }
+    else if (dim < 4096) { dim += step * 16; }
+    else { dim += step * 32; }
+}
+
+
 int main(int argc, char **argv) {
-    secondmain();
+
     double t_start, t_end;
     double gflops;
-    uint32_t dim = 0;
-    uint32_t step = 32;
-    uint32_t max = 2048;
+    dim = 0;
 
     printf("\nMatrix matrix multiply example:\n\n");
 
     while (dim < max) {
-        if (dim < 256) { dim += step; }
-        else if (dim < 512) { dim += step * 2; }
-        else if (dim < 1024) { dim += step * 4; }
-        else if (dim < 2048) { dim += step * 8; }
-        else if (dim < 4096) { dim += step * 16; }
-        else { dim += step * 32; }
+        set_dim();
 
         double *A = random_mat(dim);
         double *B = random_mat(dim);
@@ -60,27 +68,20 @@ int main(int argc, char **argv) {
         free(C);
     }
     printf("\n");
-
+    secondmain();
     return EXIT_SUCCESS;
 }
 
 
-int secondmain(){
+int secondmain() {
+
     double t_start, t_end;
     double gflops;
-    uint32_t dim = 0;
-    uint32_t step = 32;
-    uint32_t max = 2048;
-
-    printf("\nMatrix matrix multiply example:\n\n");
+    dim = 0;
+    printf("\nMatrix matrix multiply example with arrays:\n\n");
 
     while (dim < max) {
-        if (dim < 256) { dim += step; }
-        else if (dim < 512) { dim += step * 2; }
-        else if (dim < 1024) { dim += step * 4; }
-        else if (dim < 2048) { dim += step * 8; }
-        else if (dim < 4096) { dim += step * 16; }
-        else { dim += step * 32; }
+        set_dim();
 
         double **A = random_mat_A(dim);
         double **B = random_mat_A(dim);
@@ -113,4 +114,5 @@ int secondmain(){
         free(C);
     }
     printf("\n");
+    return EXIT_SUCCESS;
 }
